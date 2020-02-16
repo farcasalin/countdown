e51c33c5827b1344447e7c406150f55c8c3af036
Alin Farcas <farcashalin@gmail.com>
2020-02-16 22:11:19 +0200

    Updates

---
+++ precache-manifest.2adc7033cdab8ff65daa44f0a9d5288e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e827197600600a257f82ebcf5adf0e",
+    "url": "/countdown/index.html"
+  },
+  {
+    "revision": "4eb157bbbb80a04357d4",
+    "url": "/countdown/static/css/main.33d7136f.chunk.css"
+  },
+  {
+    "revision": "89f21d1b7d64e3b48634",
+    "url": "/countdown/static/js/2.1f569c87.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/countdown/static/js/2.1f569c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb157bbbb80a04357d4",
+    "url": "/countdown/static/js/main.0b7d71b8.chunk.js"
+  },
+  {
+    "revision": "2bb723ad4d16b1f610bf",
+    "url": "/countdown/static/js/runtime-main.64d1269a.js"
+  }
+]);
